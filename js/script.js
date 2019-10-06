6a90527c1696937206c38e82cddd48430d3384a2
DanFreder <freder.daniel@gmail.com>
2019-10-06 19:47:23 -0400

    Added wave scalar for 0 pressed state

--- js/script.js
+++ js/script.js
@@ -4,13 +4,15 @@ df website homepage
 
 "use strict";
 
-let pressed = 0;
+var pressed = 0;
 var aX = -400;
 var aY = -344;
 var bX = 0;
 var bY = 348.84;
 var cX = 400;
 var cY = -344;
+
+var angle = 0;
 
 function preload() {
   // dfLive = loadImage('assets/images/DF_liveTest.jpg');
@@ -29,20 +31,37 @@ function setup() {
 }
 
 function draw() {
-  push();
+  let wave = (sin(angle));
+  angle += 0.015;
   translate(windowWidth / 2, windowHeight / 2);
-  fill(0, 0, 0, 10);
+  push();
+  fill(0, 0, 100, 4);
   stroke(220, 20, 60, 60);
   strokeWeight(1);
   var oX = ((aX + bX + cX) / 3);
   var oY = ((aY + bY + cY) / 3);
   if (pressed === 1) {
+    var aX = -400;
+    var aY = -344;
+    var bX = 0;
+    var bY = 348.84;
+    var cX = 400;
+    var cY = -344;
     translate(pmouseX - width / 2, pmouseY - height / 2);
   }
-  rotate(radians(frameCount));
+  if (pressed === 0) {
+    var aX = -400 * wave;
+    var aY = -344;
+    var bX = 0;
+    var bY = 348.84;
+    var cX = 400 * wave;
+    var cY = -344;
+  }
+  rotate(radians(frameCount / 2));
   triangle(aX, aY, bX, bY, cX, cY);
   pop();
 }
+
 
 function mousePressed() {
   if (pressed === 0) {
